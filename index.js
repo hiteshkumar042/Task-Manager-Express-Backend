390eb243553a9dc2ec4a69fe603e747f403e5a71
hiteshkumar042 <hksolanki78@gmail.com>
2026-01-03 14:44:27 +0530

    cors issue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const mongoose = require("mongoose");
 
 app.use(express.json());
